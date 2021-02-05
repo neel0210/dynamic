88ee9b472de7f9e766ad5c516c83461270910770
cytolytic <71159394+cytoo@users.noreply.github.com>
2021-02-05 12:14:56 +0200

    use a case switch for better looking code

--- neel.sh
+++ neel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "                                                     "
 echo "             _     _                     _       _   "
-echo "  _ __  _ __(_)___| |__    ___  ___ _ __(_)_ __ | |_ "                                              
-echo " | '_ \| '__| / __| '_ \  / __|/ __| '__| | '_ \| __|"                                              
-echo " | |_) | |  | \__ \ | | | \__ \ (__| |  | | |_) | |_ "                                              
+echo "  _ __  _ __(_)___| |__    ___  ___ _ __(_)_ __ | |_ "
+echo " | '_ \| '__| / __| '_ \  / __|/ __| '__| | '_ \| __|"
+echo " | |_) | |  | \__ \ | | | \__ \ (__| |  | | |_) | |_ "
 echo " | .__/|_|  |_|___/_| |_| |___/\___|_|  |_| .__/ \__|"
 echo " |_|                                      |_|        "
 echo "                                                     "
@@ -14,36 +14,44 @@ echo "1 = Clear"
 echo "2 = Extract super.img"
 echo "3 = Pack super.img"
 echo "4 = Exit"
-read n
+read input
 
-if [ $n -eq 1 ]; then
-echo "============="
-echo "Clearing ALL"
-echo "============="
-rm *.img
-#rm *.img-old
-fi
+case $input in
+1)
+  echo "============="
+  echo "Clearing ALL"
+  echo "============="
+  rm *.img
+  #rm *.img-old
+  ;;
 
-if [ $n -eq 2 ]; then
-echo "==================="
-echo "Unpack super image"
-echo "===================" 
-./imjtool super.img extract ./PRISH
-mv super.img ./PRISH/super.img-old ; mv extracted/* .
-fi
+2)
+  echo "==================="
+  echo "Unpack super image"
+  echo "==================="
+  ./imjtool super.img extract ./PRISH
+  mv super.img ./PRISH/super.img-old ; mv extracted/* .
+  ;;
 
-if [ $n -eq 3 ]; then
-echo "================="
-echo "pack super image"
-echo "================="
-chmod a+x lpmake
-./lpmake --metadata-size 65536 --super-name super --metadata-slots 2 --device super:6539968512 --group main:4187590970 --partition system:readonly:2330120192:main --image system=./system.img --partition vendor:readonly:646946816:main --image vendor=./vendor.img --partition odm:readonly:4349952:main --image odm=./odm.img --sparse --output ./super.img
-fi
+3)
+  echo "================="
+  echo "pack super image"
+  echo "================="
+  chmod a+x lpmake
+  ./lpmake --metadata-size 65536 --super-name super --metadata-slots 2 --device super:6539968512 --group main:4187590970 --partition system:readonly:2330120192:main --image system=./system.img --partition vendor:readonly:646946816:main --image vendor=./vendor.img --partition odm:readonly:4349952:main --image odm=./odm.img --sparse --output ./super.img
+  ;;
 
-if [ $n -eq 4 ]; then
-echo "======"
-echo "exit"
-echo "======"
-./lpunpack --partition=odm super.img .
-#exit
-fi
+4)
+  echo "======"
+  echo "exit"
+  echo "======"
+  ./lpunpack --partition=odm super.img .
+  #exit
+  ;;
+
+*)
+  echo "invalid input!\nexitting..."
+  exit 1
+  ;;
+
+esac
